3c63207422e3bea1828736def3f987916b04b4f6
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-06-20 11:35:02 +0600

    ok done

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div>
       <h2 className="text-center mt-5">Not found</h2>
-      <p className="text-center">Error 404</p>
+      <p className="text-center ">Error 404</p>
     </div>
   );
 };
